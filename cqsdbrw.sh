2099e183cb335e2d9da57f9168c36ad9a8776a37
Quanhu Sheng <shengqh@gmail.com>
2018-08-03 21:41:19 +0000

    for mount bucket to compute note

---
+++ cqsdbrw.sh
@@ -0,0 +1,4 @@
+sudo umount /mnt/disks/cqsdb
+gcloud compute instances detach-disk cqscluster-controller --disk=cqsdb
+gcloud compute instances attach-disk cqscluster-controller --disk=cqsdb
+sudo mount -o discard,defaults /dev/sdb /mnt/disks/cqsdb
---
+++ gcsfuse_mount.sh
@@ -0,0 +1,2 @@
+gcsfuse --implicit-dirs vumc db
+#fusermount -u db
